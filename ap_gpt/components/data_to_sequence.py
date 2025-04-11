123c893adf91fbefa94e26e6742c18b24c2b7610
Abdoul Razac SANE <a.razacsane@gmail.com>
2025-04-11 12:07:44 +0200

    refactor(data-to-sequence): streamline initialization and sequence processing methods

--- ap_gpt/components/data_to_sequence.py
+++ ap_gpt/components/data_to_sequence.py
@@ -15,7 +15,6 @@ from ap_gpt.ap_logger import logging
 class DataToSequence:
     def __init__(self,
                  data_tokenizer_artifact : DataTokenizerArtifact,
-                 pad_token_idx : Tuple[int, int, int],
                  data_to_sequence_config : DataToSequenceConfig = DataToSequenceConfig(),
                  ) -> None:
         """
@@ -26,28 +25,16 @@ class DataToSequence:
         """
 
         self._schema_config = read_yaml_file(file_path=os.path.join(from_root(), SCHEMA_FILE_PATH))
-        self.data_tokenizer_artifact = data_tokenizer_artifact
-        self.data_to_sequence_config = data_to_sequence_config
-        self.table_name = TABLE_TRIP_NAME
         self.person_id_col = self._schema_config[TABLE_PERSON_NAME][SCHEMA_IDENTIFIER_NAME]
-        self.pad_token_idx = pad_token_idx
+        self.table_name = TABLE_TRIP_NAME
+
+        self.data_tokenizer_artifact = data_tokenizer_artifact
+        self.pad_token_idx = data_tokenizer_artifact.pad_token_idx
+        self.nb_actions = self.data_tokenizer_artifact.nb_actions
+
+        self.data_to_sequence_config = data_to_sequence_config
         self.action_nb_cols = self.data_to_sequence_config.action_nb_cols
-        self.nb_actions = None
         self.max_seq_len = self.data_to_sequence_config.max_seq_len
-        
-    def pivot_wider(self, data : pd.DataFrame) -> pd.DataFrame:
-
-        # Pivot wider actions table to have one row per person and ordered actions
-        self.nb_actions = data['person_action_id'].max()
-
-        df_actions = data.pivot(index=self.person_id_col, columns='person_action_id').reset_index()
-        df_actions.columns = [f"{x}_{y}" for x, y in df_actions.columns]
-        df_actions = df_actions.rename(columns={f"{self.person_id_col}_": self.person_id_col})
-        df_actions = df_actions.sort_values(by=[self.person_id_col])
-        df_actions = df_actions[[self.person_id_col] + np.concatenate(
-            ([[f"action_{i}", f"duration_{i}", f"distance_{i}"] for i in range(self.nb_actions)])
-        ).tolist()]
-        return df_actions
 
     def split_row_to_sequence(self,
                               sequence: Union[np.ndarray, List],
@@ -57,31 +44,31 @@ class DataToSequence:
         """
         This method of DataToSequence class is responsible for splitting the row into a sequence
         """
-        X, y = list(), list()
+
+        x_list, y_list = list(), list()
         start_idx = len(sequence) - self.nb_actions * self.action_nb_cols - 1
         end_idx = len(sequence)
         for i in range(start_idx, end_idx, self.action_nb_cols):
             seq_x, seq_y = list(sequence[:i]), list(sequence[i:i + self.action_nb_cols])
-            if not (drop_pad and (seq_y[0] in self.pad_token_idx or seq_y[1] in self.pad_token_idx or seq_y[2] in self.pad_token_idx)):
-                if len(seq_x) < self.max_seq_len:
-                    seq_x += self.pad_token_idx * ((self.max_seq_len - len(seq_x)) // len(self.pad_token_idx))
-                X.append(np.array(seq_x))
-                y.append(np.array(seq_y))
-            else:
+            if drop_pad and ((seq_y[0] in self.pad_token_idx) or (seq_y[1] in self.pad_token_idx) or (seq_y[2] in self.pad_token_idx)):
                 break
-        return np.vstack(X), np.vstack(y)
+            if len(seq_x) < self.max_seq_len:
+                seq_x += self.pad_token_idx * ((self.max_seq_len - len(seq_x)) // len(self.pad_token_idx))
+            x_list.append(np.array(seq_x))
+            y_list.append(np.array(seq_y))
+        return np.vstack(x_list), np.vstack(y_list)
 
     def split_data_to_sequence(self, data: Union[pd.DataFrame, np.array], drop_pad=True) -> Tuple[np.ndarray, np.ndarray]:
         """
         This method of DataToSequence class is responsible for splitting the data into sequences
         """
         data = data.values if isinstance(data, pd.DataFrame) else data
-        X, y = list(), list()
+        x_list, y_list = list(), list()
         for row in data:
-            x, y_ = self.split_row_to_sequence(row, drop_pad=drop_pad)
-            X.append(x)
-            y.append(y_)
-        return np.vstack(X), np.vstack(y)
+            x, y = self.split_row_to_sequence(row, drop_pad=drop_pad)
+            x_list.append(x)
+            y_list.append(y)
+        return np.vstack(x_list), np.vstack(y_list)
 
 
     def initiate_data_to_sequence(self) -> DataToSequenceArtifact:
@@ -93,11 +80,6 @@ class DataToSequence:
             logging.info("Importing datasets for sequencing")
             df_train = read_data(self.data_tokenizer_artifact.train_encoded_data_file_path)
             df_test = read_data(self.data_tokenizer_artifact.test_encoded_data_file_path)
-
-            # Pivot wider
-            logging.info("Pivoting wider the actions table")
-            df_train = self.pivot_wider(df_train)
-            df_test = self.pivot_wider(df_test)
 
             # Split data to sequence
             logging.info("Splitting data to sequence")

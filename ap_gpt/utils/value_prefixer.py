09579a5b44f1165e25a94634d32af84daa547096
Abdoul Razac SANE <a.razacsane@gmail.com>
2025-04-17 18:26:45 +0200

    refactor(value-prefixer): simplify ValuePrefixer class by converting methods to standalone functions

--- ap_gpt/utils/value_prefixer.py
+++ ap_gpt/utils/value_prefixer.py
@@ -1,29 +1,21 @@
+import pandas as pd
 from typing import List
 
-class ValuePrefixer:
-    """
-        Add colname to values like a prefix
-    """
+def transform(data, col_name:str) -> pd.Series:
+    return data[col_name].apply(lambda x: f"{col_name}_{x}")
 
-    @staticmethod
-    def transform(data, col_name:str):
-        return data[col_name].apply(lambda x: f"{col_name}_{x}")
+def inverse_transform(data, col_name) -> pd.Series:
+    return data[col_name].apply(lambda x: x.replace(f"{col_name}_", ""))
 
-    @staticmethod
-    def inverse_transform(data, col_name):
-        return data[col_name].apply(lambda x: x.replace(f"{col_name}_", ""))
+def transform_all(data, exclude: List[str] = [], include: List[str] =[]) -> pd.DataFrame:
+    for col in data.columns:
+        if col not in exclude and (len(include) == 0 or col in include):
+            data[col] = transform(data, col)
+    return data
 
-    @staticmethod
-    def transform_all(data, exclude: List[str] = [], include: List[str] =[]):
-        for col in data.columns:
-            if col not in exclude and (len(include) == 0 or col in include):
-                data[col] = ValuePrefixer.transform(data, col)
-        return data
-
-    @staticmethod
-    def inverse_transform_all(data, exclude: List[str] =[], include: List[str] =[]):
-        for col in data.columns:
-            if col not in exclude and (len(include) == 0 or col in include):
-                data[col] = ValuePrefixer.inverse_transform(col)
-        return data
+def inverse_transform_all(data, exclude: List[str] =[], include: List[str] =[]) -> pd.DataFrame:
+    for col in data.columns:
+        if col not in exclude and (len(include) == 0 or col in include):
+            data[col] = inverse_transform(col)
+    return data
 

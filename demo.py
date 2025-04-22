e49bcadf1a354ce73b45aaa81acd5c7993382867
Abdoul Razac SANE <a.razacsane@gmail.com>
2025-04-23 01:34:10 +0200

    feat(demo): initialize TrainPipeline with TrainingPipelineConfig for ActionGPT

--- demo.py
+++ demo.py
@@ -1,7 +1,10 @@
+from ap_gpt.entity.config_entity import TrainingPipelineConfig
 from ap_gpt.pipelines.train_pipeline import TrainPipeline
 
 # Clear a
 
-train_pipeline = TrainPipeline()
+train_pipeline = TrainPipeline(
+    training_pipeline_config=TrainingPipelineConfig(model_name="ActionGPT"),
+)
 
 train_pipeline.run_pipeline()

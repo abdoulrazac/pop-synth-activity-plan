3f3a1b6aef16ac49d43a9bafb91eb1df482806d3
Abdoul Razac SANE <a.razacsane@gmail.com>
2025-05-03 08:59:52 +0200

    Add training scripts for GPT and LSTM models with updated configurations

---
+++ train_lstm.py
@@ -0,0 +1,6 @@
+from ap.constants import ModelName
+from ap.entity.config_entity import TrainingPipelineConfig
+from ap.pipelines.train_pipeline import TrainPipeline
+
+train_pipeline = TrainPipeline(training_pipeline_config=TrainingPipelineConfig(model_name=ModelName.LSTM))
+train_pipeline.run_pipeline()
